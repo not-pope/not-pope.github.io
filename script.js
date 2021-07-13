e0741a929e997138f5822f614d7a5f392d248d0b
Luka Ivanovic <ivanovic@tutanota.de>
2021-07-13 12:37:26 +0200

    eya

---
+++ script.js
@@ -0,0 +1,10 @@
+function rot(s, i) {
+    return s.replace(/[a-zA-Z]/g, function (c) {
+        return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + i) ? c : c - 26);
+    });
+}
+
+function update() {
+    document.getElementById('output').value = rot(document.getElementById('input').value, +document.getElementById('rot-value').value);
+    //                                                                                    +somethin is converting somethin to a number
+}
